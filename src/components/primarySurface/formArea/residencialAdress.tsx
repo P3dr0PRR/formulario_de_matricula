df751c76d5f5e14b59d4facd2c9c2c0d82cd3c8b
Pedro Paulo <pedropaulo@interlinksistemas.com.br>
2026-03-29 19:53:07 -0300

    feat: Endereço residencial concluido

--- src/components/primarySurface/formArea/residencialAdress.tsx
+++ src/components/primarySurface/formArea/residencialAdress.tsx
@@ -5,7 +5,7 @@ export function ResidencialAdress() {
         <h3 className="text-color-primary">Endereço residencial</h3>
       </div>
       <fieldset className="flex flex-col gap-[16px] ">
-        {/* <div className="input-wrapper">
+        <div className="input-wrapper">
           <label htmlFor="street">CEP</label>
           <input
             className="border border-stroke-default rounded px-3 py-2 placeholder:text-text-tertiary focus:outline-[#F67841]"
@@ -15,17 +15,17 @@ export function ResidencialAdress() {
             placeholder="Digite o CEP"
             required
           />
-        </div> */}
+        </div>
 
         <div className="input-wrapper grid grid-cols-2">
           <div className="flex flex-col gap-4 ">
-            <label htmlFor="street">CEP</label>
+            <label htmlFor="street">Rua</label>
             <input
               className="border border-stroke-default rounded px-3 py-2 placeholder:text-text-tertiary focus:outline-[#F67841]"
               type="text"
-              id="cep"
-              name="cep"
-              placeholder="Digite o CEP"
+              id="street"
+              name="street"
+              placeholder="Rua das Flores"
               required
             />
           </div>
