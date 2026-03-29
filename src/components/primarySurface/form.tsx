cc3c86e783e9c59961e36ad697cd2b5c0199dbcc
Pedro Paulo <pedropaulo@interlinksistemas.com.br>
2026-03-29 19:51:21 -0300

    feat: informações da criança concluido

--- src/components/primarySurface/form.tsx
+++ src/components/primarySurface/form.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./header";
 import { KidInfos } from "./formArea/kidInfos";
+import { ResidencialAdress } from "./formArea/residencialAdress";
 export function Form() {
   return (
     <section className="w-full flex flex-col gap-4 p-6 md:p-[4rem] scroll-auto">
       <Header />
-      <KidInfos />
+      <form className="flex flex-col gap-32">
+        <KidInfos />
+        <ResidencialAdress />
+      </form>
     </section>
   );
 }
---
+++ src/components/primarySurface/formArea/residencialAdress.tsx
@@ -0,0 +1,71 @@
+export function ResidencialAdress() {
+  return (
+    <section className="w-full flex flex-col gap-[4rem]">
+      <div className="flex flex-col ">
+        <h3 className="text-color-primary">Endereço residencial</h3>
+      </div>
+      <fieldset className="flex flex-col gap-[16px] ">
+        {/* <div className="input-wrapper">
+          <label htmlFor="street">CEP</label>
+          <input
+            className="border border-stroke-default rounded px-3 py-2 placeholder:text-text-tertiary focus:outline-[#F67841]"
+            type="text"
+            id="cep"
+            name="cep"
+            placeholder="Digite o CEP"
+            required
+          />
+        </div> */}
+
+        <div className="input-wrapper grid grid-cols-2">
+          <div className="flex flex-col gap-4 ">
+            <label htmlFor="street">CEP</label>
+            <input
+              className="border border-stroke-default rounded px-3 py-2 placeholder:text-text-tertiary focus:outline-[#F67841]"
+              type="text"
+              id="cep"
+              name="cep"
+              placeholder="Digite o CEP"
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-4 w-[176px]  justify-self-end">
+            <label htmlFor="street">Número</label>
+            <input
+              className="border border-stroke-default rounded px-3 py-2 placeholder:text-text-tertiary focus:outline-[#F67841]"
+              type="text"
+              id="number"
+              name="number"
+              placeholder="123"
+              required
+            />
+          </div>
+        </div>
+        <div className="input-wrapper  grid grid-cols-2">
+          <div className="flex flex-col gap-4 ">
+            <label htmlFor="street">Cidade</label>
+            <input
+              className="border border-stroke-default rounded px-3 py-2 placeholder:text-text-tertiary focus:outline-[#F67841]"
+              type="text"
+              id="city"
+              name="city"
+              placeholder="São Paulo"
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-4 w-[176px] justify-self-end">
+            <label htmlFor="street">Estado</label>
+            <input
+              className="border border-stroke-default rounded px-3 py-2 placeholder:text-text-tertiary focus:outline-[#F67841]"
+              type="text"
+              id="state"
+              name="state"
+              placeholder="SP"
+              required
+            />
+          </div>
+        </div>
+      </fieldset>
+    </section>
+  );
+}
