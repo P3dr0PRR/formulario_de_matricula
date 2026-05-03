b824300c4c07b94233b26f661aef30440008c672
Pedro Paulo <pedropaulo@interlinksistemas.com.br>
2026-05-03 16:53:49 -0300

    feat: translate Header to english

--- src/components/primarySurface/header.tsx
+++ src/components/primarySurface/header.tsx
@@ -4,14 +4,14 @@ export function Header() {
   return (
     <header className="surface-primary p-4 gap-[16px] flex flex-col justify-start">
       <div className="flex gap-[8px] hidden md:block">
-        <img src={arrowLeft} alt="Seta para esquerda" />
-        <p className=" text-color-tertiary">Voltar</p>
+        <img src={arrowLeft} alt="Arrow left" />
+        <p className=" text-color-tertiary">Back</p>
       </div>
       <div className="flex flex-col gap-[8px]">
-        <h2 className="flex justify-start">Formulário de Matrícula</h2>
+        <h2 className="flex justify-start">Enrollment Form</h2>
         <p className="text-color-tertiary">
-          Preencha os dados abaixo para matricular seu filho na escola de
-          educação infantil Estrelas do Amanhã.
+          Fill in the information below to enroll your child at Estrelas do
+          Amanhã Kindergarten.
         </p>
       </div>
     </header>
